7184cab48244a7b43cc7dd17be8c2dc720bd49a3
Ubuntu <ecobici@msmlserver.m1lrtav2uuhedmnz05ukgcfqac.jx.internal.cloudapp.net>
2018-05-02 04:37:09 +0000

    Trying to load ecobici data.

--- load_data.R
+++ load_data.R
@@ -1,1 +1,24 @@
-# Yes, yes, by all means
+# Comment!
+# File paths
+inputFile <- file.path('/mnt/ecobici-data', "ecobici_2010_2017.csv")
+outputFile <- file.path('./data', "ecobici_2010_2017.xdf")
+
+# Read first 5 lines of CSV
+ecobici_data_5lines <- read.csv(inputFile, nrow=5, stringsAsFactors = F)
+
+# Details of input file
+ecobici_datasource_details <- RxTextData(inputFile, stringsAsFactors = F, rowsToSniff = 5000)
+
+# Import the data into the xdf file
+ecobici_datasource <- rxImport(inData = ecobici_datasource_details, 
+                               outFile = outputFile, 
+                               overwrite = TRUE, 
+                               numRows = 5,
+                               varsToDrop = c('fecha_arribo_completa','fecha_retiro_completa','hora_arribo_copy', 'hora_retiro_copy'),
+                               transforms = list(fecha_retiro=as.Date(fecha_retiro, format='%Y-%m-%d'),
+                                                 fecha_arribo=as.Date(fecha_arribo, format='%Y-%m-%d')))
+
+
+
+
+
