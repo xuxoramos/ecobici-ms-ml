2f2ef88137f8f1c98fc5094d85685b16ad987600
Ubuntu <ecobici@ecobici-ds-vm.m1lrtav2uuhedmnz05ukgcfqac.jx.internal.cloudapp.net>
2018-05-14 04:59:48 +0000

    Transformed from CSV to XDF for analysis.

--- load_data.R
+++ load_data.R
@@ -1,22 +1,29 @@
 # Comment!
 # File paths
-inputFile <- file.path('/mnt/ecobici-data', "ecobici_2010_2017.csv")
-outputFile <- file.path('./data', "ecobici_2010_2017.xdf")
+ecobiciinputFile <- file.path('/mnt/ecobici-data', "ecobici_2010_2017-final.csv")
+ecobicioutputFile <- file.path('./data/', "ecobici_2010_2017-final.xdf")
 
-# Read first 5 lines of CSV
-ecobici_data_5lines <- read.csv(inputFile, nrow=5, stringsAsFactors = F)
+#testinputfile <- file.path('/mnt/ecobici-data', "test-final.csv")
+#testoutputfile <- file.path('./data/', "test-final.xdf")
 
-# Details of input file
-ecobici_datasource_details <- RxTextData(inputFile, stringsAsFactors = F, rowsToSniff = 5000)
+#testdata <- rxImport(inData = testinputfile,
+#                     outFile = testoutputfile, 
+#                     overwrite = TRUE, 
+#                     varsToDrop = c('fecha_arribo_completa',
+#                                    'fecha_retiro_completa',
+#                                    'hora_arribo_copy', 
+#                                    'hora_retiro_copy'))
 
 # Import the data into the xdf file
-ecobici_datasource <- rxImport(inData = ecobici_datasource_details, 
-                               outFile = outputFile, 
+ecobici_datasource <- rxImport(inData = ecobiciinputFile,
+                               outFile = ecobicioutputFile, 
                                overwrite = TRUE, 
-                               numRows = 5,
+#                               numRows = 5,
                                varsToDrop = c('fecha_arribo_completa','fecha_retiro_completa','hora_arribo_copy', 'hora_retiro_copy'),
                                transforms = list(fecha_retiro=as.Date(fecha_retiro, format='%Y-%m-%d'),
                                                  fecha_arribo=as.Date(fecha_arribo, format='%Y-%m-%d')))
+
+
 
 
 
