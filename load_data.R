3c0040ba485042b42daebd5caf565ef38bf11a36
Ubuntu <ecobici@ecobici-ds-vm.m1lrtav2uuhedmnz05ukgcfqac.jx.internal.cloudapp.net>
2018-05-14 05:15:17 +0000

    Managed to load file and transform it to XDF.

--- load_data.R
+++ load_data.R
@@ -3,22 +3,11 @@
 ecobiciinputFile <- file.path('/mnt/ecobici-data', "ecobici_2010_2017-final.csv")
 ecobicioutputFile <- file.path('./data/', "ecobici_2010_2017-final.xdf")
 
-#testinputfile <- file.path('/mnt/ecobici-data', "test-final.csv")
-#testoutputfile <- file.path('./data/', "test-final.xdf")
-
-#testdata <- rxImport(inData = testinputfile,
-#                     outFile = testoutputfile, 
-#                     overwrite = TRUE, 
-#                     varsToDrop = c('fecha_arribo_completa',
-#                                    'fecha_retiro_completa',
-#                                    'hora_arribo_copy', 
-#                                    'hora_retiro_copy'))
-
 # Import the data into the xdf file
+# It'll take around 40 min.
 ecobici_datasource <- rxImport(inData = ecobiciinputFile,
                                outFile = ecobicioutputFile, 
                                overwrite = TRUE, 
-#                               numRows = 5,
                                varsToDrop = c('fecha_arribo_completa','fecha_retiro_completa','hora_arribo_copy', 'hora_retiro_copy'),
                                transforms = list(fecha_retiro=as.Date(fecha_retiro, format='%Y-%m-%d'),
                                                  fecha_arribo=as.Date(fecha_arribo, format='%Y-%m-%d')))
