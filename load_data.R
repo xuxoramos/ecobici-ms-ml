a6b011e9704e2f6ee8d032b0bc00d8f8f8ae34b4
Ubuntu <ecobici@ecobici-ds-vm.m1lrtav2uuhedmnz05ukgcfqac.jx.internal.cloudapp.net>
2018-05-17 05:14:36 +0000

    Little steps towards functionality.

--- load_data.R
+++ load_data.R
@@ -5,13 +5,18 @@ ecobicioutputFile <- file.path('./data/', "ecobici_2010_2017-final.xdf")
 
 # Import the data into the xdf file
 # It'll take around 40 min.
-ecobici_datasource <- rxImport(inData = ecobiciinputFile,
+rxImport(inData = ecobiciinputFile,
                                outFile = ecobicioutputFile, 
                                overwrite = TRUE, 
                                varsToDrop = c('fecha_arribo_completa','fecha_retiro_completa','hora_arribo_copy', 'hora_retiro_copy'),
                                transforms = list(fecha_retiro=as.Date(fecha_retiro, format='%Y-%m-%d'),
                                                  fecha_arribo=as.Date(fecha_arribo, format='%Y-%m-%d')))
 
+# Read in XDF file we have just written
+ecobici_datasource <- rxReadXdf(file = ecobicioutputFile, numRows = 3740000)
+
+# Summary of the dataset
+rxSummary(~., data = flight_mrs, blocksPerRead = 2)
 
 
 
